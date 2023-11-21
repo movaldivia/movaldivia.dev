d4ba6ba6facc7d022350dac2b40f9cd0aaabd6cd
movaldivia <movaldivia@uc.cl>
2023-11-21 11:31:04 -0300

    feat: add headers and image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -273,13 +273,13 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Software engineer and <br /> co-founder.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Mauricio, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Mauricio, a software engineer and entrepreneur. I’m the
+            co-founder and ex-CTO of WallState, where we developed an investment
+            app that permits users to invest from one dollar in real estate.
+            Always looking for new challenges.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
