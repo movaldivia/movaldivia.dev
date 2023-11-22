5737f80f6b2d44bf08530c9f35879aacdc6acaaa
movaldivia <movaldivia@uc.cl>
2023-11-22 20:52:10 -0300

    feat: add pdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,11 +69,11 @@ function About() {
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>Hi!</p>
             <p>
-              I'm Mauricio, a passionate software engineer. I have programmed
-              many different things, from a simple hello world to smart
-              contracts. What I love about programming is that you don't have
-              limits in your creation. You need a laptop with an internet
-              connection and your own time.
+              I&apos;m Mauricio, a passionate software engineer. I have
+              programmed many different things, from a simple hello world to
+              smart contracts. What I love about programming is that you
+              don&apos;t have limits in your creation. You need a laptop with an
+              internet connection and your own time.
             </p>
             <p>
               Over my years of startup environment experience, I have learned a
@@ -297,10 +297,12 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
-        Download CV
-        <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-      </Button>
+      <a href="/cv.pdf" target="_blank" rel="noopener noreferrer">
+        <Button variant="secondary" className="group mt-6 w-full">
+          Download CV
+          <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+        </Button>
+      </a>
     </div>
   )
 }
