1d2a6e41c0edde70b34f4f42125c34f079081848
movaldivia <movaldivia@uc.cl>
2023-11-22 20:42:25 -0300

    feat: add my own story

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,31 +67,23 @@ function About() {
             change the future.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <p>Hi!</p>
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              I'm Mauricio, a passionate software engineer. I have programmed
+              many different things, from a simple hello world to smart
+              contracts. What I love about programming is that you don't have
+              limits in your creation. You need a laptop with an internet
+              connection and your own time.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              Over my years of startup environment experience, I have learned a
+              lot of things; I consider myself a generalist who can adapt
+              quickly. I can develop an MVP with good enough UX/UI, frontend,
+              backend, DevOps, ads, and more. Very customer-focused.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
-            </p>
-            <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              I&apos;m currently looking for new challenges, so if you want to
+              contact me for anything, please email me!
             </p>
           </div>
         </div>
@@ -134,15 +126,14 @@ function About() {
 const projects = [
   {
     name: 'WallState',
-    description:
-      'Invest in US real estate from one dollar and from Latam. More than ten thousands users.',
+    description: 'Invest in US real estate from one dollar and Latam.',
     link: { href: 'https://www.wallstate.cl/', label: 'wallstate.cl' },
     logo: logoWallState,
   },
   {
     name: 'Reity',
     description:
-      'Buy percentages of Chile real estate using blockchain technology.',
+      "Buy percentages of Chile's real estate using blockchain technology.",
     link: { href: 'https://www.reity.cl/', label: 'reity.cl' },
     logo: logoReity,
   },
@@ -337,7 +328,6 @@ export default async function Home() {
             I’m Mauricio, a software engineer and entrepreneur. I’m the
             co-founder and ex-CTO of WallState, where we developed an investment
             app that permits users to invest from one dollar in real estate.
-            Always looking for new challenges.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
